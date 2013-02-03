b91eaa59fc232bf66640ce15344f810b662cdef9
Christian Ramon <rchristian.ramon@gmail.com>
2013-02-03 16:52:09 -0500

    Add: rails init files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wordsmark::Application.config.secret_token = 'd995e8c9c5116f5dbeba45ea2e54e2d2889d121b1d8a661b037978bba56abd59f6e69d8ee96e9991fe1219747ed1d87a3c02f9374c717eab46416a7c116fad28'
