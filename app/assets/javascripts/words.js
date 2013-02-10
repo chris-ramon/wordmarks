5c70a998269800f4d8b754a8b437586b4b7d78e9
Christian Ramon <rchristian.ramon@gmail.com>
2013-02-10 18:30:51 -0500

    Upd: words javascript, refactoring and adding new functionality

--- app/assets/javascripts/words.js
+++ app/assets/javascripts/words.js
@@ -1,24 +1,86 @@
-window.wordsmark = {};
 $(function () {
-    var practice_btn = $('#practice-btn'),
-        practice_wrapper = $('#practice-wrapper'),
-        word_wrapper = $('#word-wrapper'),
-        translation_wrapper = $('#translation-wrapper'),
-        result_wrapper = $('#result-wrapper');
-
-    // bindings.
-    practice_btn.on('click', initialize_practice);
-
-    // bindings functions.
-    function initialize_practice() {
-        var words = get_words();
-        word_wrapper.append(words[0].word);
-
+    var config = {
+        practice_btn : $('#practice-btn'),
+        practice_wrapper : $('#practice-wrapper'),
+        result_wrapper : $('#result-wrapper'),
+        word_wrapper : $('#word-wrapper'),
+        translation_wrapper : $('#translation-wrapper'),
+        translation_input : $('#translation-input'),
+        finish_wrapper: $('#finish-wrapper')
     }
-
-    function get_words() {
-        return [{'word': 'vetted'}, {'word': 'daunting'}]
-    }
+    window.Wordmark.initialize(config);
 });
+window.Wordmark = {
+    initialize: function (config){
+        this.config = config;
+        this.bindEvents();
+    },
+    bindEvents: function (){
+        // bindings.
+        this.config.practice_btn.on('click', this.initialize_practice);
+        this.config.translation_input.on('keyup', this.verify_translation);
+    },
+    // bindings functions.
+    initialize_practice: function () {
+        var self = Wordmark;
+        self.words = self.get_words();
+        self.current_word_index = 0;
+        // cleaning dom wrappers
+        self.config.result_wrapper.html('');
+        self.config.finish_wrapper.html('');
+        // end cleaning dom wrappers
+        self.set_word();
+    },
+    set_word: function() {
+        var i = this.current_word_index;
+        var words_size = this.words.length;
+        if (i < words_size) {
+            var word = this.words[i];
+            this.current_word = word.word;
+            this.current_word_translations = word.translations;
+            // view: refactor
+            this.config.word_wrapper.html(this.current_word);
+        }
+        else {
+            this.finish_practice();
+        }
+    },
+    // verifying if the translation from the input
+    // is correct
+    verify_translation: function (e) {
+        var self = Wordmark;
+        var _translations = self.current_word_translations;
 
+        var el = $(this);
+        var words = el.val();
 
+        for(i in _translations) {
+            if ( words == _translations[i] ) {
+                // view: refactor
+                self.config.result_wrapper.html('Correct !');
+                self.current_word_index += 1;
+                self.set_word();
+                break;
+            }
+            else
+                self.config.result_wrapper.html('Incorrect !');
+        }
+    },
+    // API
+    get_words: function () {
+        return [
+            {
+                'word': 'vetted',
+                'translations': ['vetados', 'examinados']
+            },
+            {
+                'word': 'daunting',
+                'translations': ['desalentador', 'intimidante']
+            }
+        ]
+    },
+    finish_practice: function (){
+        this.config.translation_input.off('keyup', this.verify_translation);
+        this.config.finish_wrapper.html('Practice finished !');
+    }
+};
